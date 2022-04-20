f3a4a7826a0f1ed07ce7cae8e53ec8864bef8492
Nathan575 <59374917+Nathan575@users.noreply.github.com>
2022-04-20 08:57:32 -0500

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,11 +1,21 @@
 #include <iostream>
-
 #include  "student.h"
 #include  "classroom.h"
+#include "linkedlist.h"
 
 
 int main() {
-  
+  Classroom* c = new Classroom("suns in fo");
+  LinkedList* l = new LinkedList();
   std::cout << "Hello World!\n";
 
+  //add
+  
+  l->add(*c);
+  std::cout<<l->get(0)->classroom.name;
+  l->remove(0);
+  if(l->head == nullptr){
+    std::cout<<"this code sucks";
+  }
+  
 }

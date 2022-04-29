8a7179a60379ebd03790e7495d1208404deb02da
Nathan575 <59374917+Nathan575@users.noreply.github.com>
2022-04-29 09:35:38 -0500

    display + blackbox classroom testing

--- classroom.h
+++ classroom.h
@@ -11,14 +11,14 @@ class Classroom{
   public:
     int posGridSize;
     std::string name;
-    int positions[100][100];
+    int positions[2][2];
     int ids[100][100];
     char riskCategory;
     Classroom(){
       posGridSize = 100;
     }
     Classroom(std::string n){
-      posGridSize = 100;
+      posGridSize = 2;
       name = n;
     }
 };
--- main.cpp
+++ main.cpp
@@ -2,20 +2,52 @@
 #include  "student.h"
 #include  "classroom.h"
 #include "linkedlist.h"
+#include <bits/stdc++.h>
 
 
 int main() {
-  Classroom* c = new Classroom("suns in fo");
-  LinkedList* l = new LinkedList();
-  std::cout << "Hello World!\n";
+  //Classroom* c = new Classroom("suns in fo");
+  //LinkedList* l = new LinkedList();
 
-  //add
-  
-  l->add(*c);
+  // Add
+  /*l->add(*c);
   std::cout<<l->get(0)->classroom.name;
   l->remove(0);
   if(l->head == nullptr){
     std::cout<<"this code sucks";
-  }
+  }*/
+
+  std::cout << "Hello World!\n";
+  Student* joe = new Student();
+  
+  
+  LinkedList* schedule = new LinkedList();
+
+  int arr[2]][2] = {{13810,1230},{12132,12043}};
+  
+
+  Classroom* a1 = new Classroom("Advanced CS");
+  a1->positions = arr;
+  Classroom* a2 = new Classroom("APHUG");
+  Classroom* a3 = new Classroom("AP Physics");
+  Classroom* a4 = new Classroom("Digital Electronics");
+  Classroom* b5 = new Classroom("Diffeq");
+  Classroom* b6 = new Classroom("AP Lang");
+  Classroom* b7 = new Classroom("AP Spanish");
+  Classroom* b8 = new Classroom("WPP");
+
+  schedule->add(*a1);
+  schedule->add(*a2);
+  schedule->add(*a3);
+  schedule->add(*a4);
+  schedule->add(*b5);
+  schedule->add(*b6);
+  schedule->add(*b7);
+  schedule->add(*b8);
+
+
+  
+  joe->schedule = *schedule;
+  joe->schedule.display();
   
 }

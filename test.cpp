26087bb9d141a061083d1104cba5628b76c30df2
Nathan575 <59374917+Nathan575@users.noreply.github.com>
2022-05-02 09:43:02 -0500

    blackbox testing implementation

---
+++ test.cpp
@@ -0,0 +1,53 @@
+/*#include <iostream>
+#include  "student.h"
+#include  "classroom.h"
+#include "linkedlist.h"
+#include <bits/stdc++.h>
+
+
+int main() {
+  //Classroom* c = new Classroom("suns in fo");
+  //LinkedList* l = new LinkedList();
+
+  // Add
+  /*l->add(*c);
+  std::cout<<l->get(0)->classroom.name;
+  l->remove(0);
+  if(l->head == nullptr){
+    std::cout<<"this code sucks";
+  }
+  LinkedList students = new LinkedList();
+  std::cout << "Hello World!\n";
+  Student* joe = new Student();
+  
+  
+  LinkedList* globalSchedule = new LinkedList();
+
+  int arr[2][2] = {{13810,1230},{12132,12043}};
+  
+
+  Classroom* a1 = new Classroom("Advanced CS");
+  a1->setPositions(arr);
+  Classroom* a2 = new Classroom("APHUG");
+  Classroom* a3 = new Classroom("AP Physics");
+  Classroom* a4 = new Classroom("Digital Electronics");
+  Classroom* b5 = new Classroom("Diffeq");
+  Classroom* b6 = new Classroom("AP Lang");
+  Classroom* b7 = new Classroom("AP Spanish");
+  Classroom* b8 = new Classroom("WPP");
+
+  schedule->add(*a1);
+  schedule->add(*a2);
+  schedule->add(*a3);
+  schedule->add(*a4);
+  schedule->add(*b5);
+  schedule->add(*b6);
+  schedule->add(*b7);
+  schedule->add(*b8);
+
+  
+  
+  joe->schedule = *schedule;
+  joe->schedule.display();
+  a1->display();
+}*/

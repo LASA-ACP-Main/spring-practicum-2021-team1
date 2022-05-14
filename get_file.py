6a6b5c283bf5662acea675758ddae1087350d06b
EthanIsDead <78391194+EthanIsDead@users.noreply.github.com>
2022-05-14 16:14:19 -0500

    Add files via upload

--- get_file.py
+++ get_file.py
@@ -1,6 +1,19 @@
 import gdown
 
-url = "https://drive.google.com/uc?export=download&id=1JL9qzg3_hOiRnM7NGx-aNFR8PX9WokaT"
+sharing_url = "https://drive.google.com/file/d/1JL9qzg3_hOiRnM7NGx-aNFR8PX9WokaT/view?usp=sharing"
+
+file_id = ""
+for i in range(len(sharing_url)):
+    if i > 31:
+        if not sharing_url[i] == "/":
+            file_id += sharing_url[i]
+        else:
+            break
+
+downloadable_url = "https://drive.google.com/uc?export=download&id=" + file_id; 
+downloadable_url; 
+
+print(downloadable_url)
 output = "file.csv"
-gdown.download(url, output)
+gdown.download(downloadable_url, output)
 
